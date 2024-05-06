af0ddc39585c29adb17054f8e4e0c16c011a8849
nathaliasantoscruz <169051128+nathaliasantoscruz@users.noreply.github.com>
2024-05-06 08:57:11 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+//preparar e rodar um codigo
+function setup() {
+  createCanvas(400, 200);//criar tela
+  background ("pink");
+}
+
+//responsável por desenhar
+function draw() {
+stroke ("purple");
+ fill ("red");   
+      
+  if (mouseIsPressed){
+     rect (mouseX,mouseY,40,40) ;
+  } 
+  } 
+      
+      
